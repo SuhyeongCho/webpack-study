1ef4035b27da8a5bda726587998397940e6f7ccb
SuhyeongCho <ch5951130@naver.com>
2021-01-05 15:53:46 +0900

    data file 로드

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,8 @@ module.exports = {
     rules: [
       { test: /\.css$/i, use: ["style-loader", "css-loader"] },
       { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: "asset/resource" },
+      { test: /\.(csv|tsv)$/i, use: ["csv-loader"] },
+      { test: /\.xml$/i, use: ["xml-loader"] },
     ],
   },
 };
