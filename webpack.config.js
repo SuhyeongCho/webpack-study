191a12c07ae29fad932b5cfff22d9db215debb86
SuhyeongCho <ch5951130@naver.com>
2021-01-11 17:18:12 +0900

    code splitting 및 caching 연습

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "[name].[contenthash].bundle.js",
+    filename: "[name].[chunkhash].bundle.js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
   },
